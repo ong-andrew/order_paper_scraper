efc5fb741680888364119ee9e79817119a6d447e
Andrew34000 <79370960+ong-andrew@users.noreply.github.com>
2021-11-14 10:41:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from scrapper import scrapper
+from scraper import scraper
 from compare_txt import compare_txt
 from send_email import send_email
 
 if __name__ == '__main__':
-    scrapper()
+    scraper()
     x = compare_txt()
     if x != None:
         send_email(x)
